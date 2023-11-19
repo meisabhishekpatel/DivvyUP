db92af9c224e57f0cd0f1fea5d39b7190b250296
meisabhishekpatel <ap9506332677@gmail.com>
2023-11-19 11:02:24 +0530

    client:email validation
    Signed-off-by: meisabhishekpatel <ap9506332677@gmail.com>

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import axios from "axios"
+import axios from "axios";
 import { useSpring, animated } from "react-spring";
-
 
 export default function Login() {
     const [showPassword, setShowPassword] = useState(false);
     const [user, setUser] = useState({
-        email: "", password: ""
+        email: "",
+        password: "",
     });
+    const [isEmailValid, setIsEmailValid] = useState(true); // New state for email validation
     const navigate = useNavigate();
     let name, value;
 
@@ -32,62 +33,67 @@ export default function Login() {
         try {
             name = e.target.name;
             value = e.target.value;
-            setUser({ ...user, [name]: value, })
+            setUser({ ...user, [name]: value });
+
+            // Email validation check
+            if (name === "email") {
+                const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+                setIsEmailValid(emailRegex.test(value));
+            }
         } catch (e) {
             console.log(e);
         }
-
-    }
-
-
+    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         const { email, password } = user;
+
+        // Check email validation before making the API request
+        if (!isEmailValid) {
+            alert("Please enter a valid email address");
+            return;
+        }
+
         try {
-            const res = await axios.post("http://localhost:3000/user/login", { email, password })
+            const res = await axios.post("http://localhost:3000/user/login", { email, password });
             if (!res.data.error) {
                 alert(res.data.message);
-                navigate("/home")
-            }
-            else {
-                alert(res.data.error)
+                navigate("/home");
+            } else {
+                alert(res.data.error);
             }
         } catch (err) {
-            alert(err.response.data.error)
+            alert(err.response.data.error);
         }
-    }
+    };
 
     return (
         <animated.div style={slideIn} className="flex flex-col items-center min-h-screen pt-6 sm:justify-center sm:pt-0 bg-gray-50">
             <animated.div style={fadeIn} className="w-full px-6 py-4 mt-6 overflow-hidden bg-gray-50 sm:max-w-lg sm:rounded-lg">
                 <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
-                    <div className="w-full p-6  m-auto bg-white rounded-md shadow-xl lg:max-w-xl">
-                        <h1 className="text-3xl font-semibold text-center text-blue-700 uppercase">
-                            Sign in
-                        </h1>
+                    <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl lg:max-w-xl">
+                        <h1 className="text-3xl font-semibold text-center text-blue-700 uppercase">Sign in</h1>
                         <form className="mt-6">
                             <div className="mb-2">
-                                <label
-                                    for="email"
-                                    className="block text-sm font-semibold text-gray-800"
-                                >
+                                <label htmlFor="email" className="block text-sm font-semibold text-gray-800">
                                     Email
                                 </label>
                                 <input
-                                    placeholder='Email'
+                                    placeholder="Email"
                                     type="email"
                                     name="email"
                                     value={user.email}
                                     onChange={handleInputs}
-                                    className="block w-full px-4 py-2 mt-2 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                                    className={`block w-full px-4 py-2 mt-2 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 ${!isEmailValid ? "border-red-500" : ""
+                                        }`}
                                 />
+                                {!isEmailValid && (
+                                    <p className="text-red-500 text-sm">Please enter a valid email address</p>
+                                )}
                             </div>
                             <div className="mb-2">
-                                <label
-                                    for="password"
-                                    className="block text-sm font-semibold text-gray-800"
-                                >
+                                <label htmlFor="password" className="block text-sm font-semibold text-gray-800">
                                     Password
                                 </label>
                                 <div className="relative">
@@ -109,7 +115,11 @@ export default function Login() {
                                 </div>
                             </div>
                             <div className="mt-6">
-                                <button onClick={handleSubmit} className="w-full px-4 py-2 text-decoration-none tracking-wide text-white transition-colors duration-200 transform bg-blue-700 rounded-md hover:bg-blue-800 focus:outline-none focus:bg-blue-600">
+                                <button
+                                    onClick={handleSubmit}
+                                    disabled={!isEmailValid} // Disable button if email is not valid
+                                    className="w-full px-4 py-2 text-decoration-none tracking-wide text-white transition-colors duration-200 transform bg-blue-700 rounded-md hover:bg-blue-800 focus:outline-none focus:bg-blue-600"
+                                >
                                     Login
                                 </button>
                             </div>
@@ -119,13 +129,17 @@ export default function Login() {
                         </div>
                         <div className="flex mt-4 gap-x-2">
                             <button
-                                class="px-4 py-2 border flex w-full gap-2 items-center justify-center border-slate-700 rounded-lg text-black-200 hover:border-slate-500 hover:text-slate-300 hover:shadow transition duration-150">
-                                <img class="w-6 h-6" src="/assets/google.svg" loading="lazy"
-                                    alt="google logo" />
+                                className="px-4 py-2 border flex w-full gap-2 items-center justify-center border-slate-700 rounded-lg text-black-200 hover:border-slate-500 hover:text-slate-300 hover:shadow transition duration-150"
+                            >
+                                <img
+                                    className="w-6 h-6"
+                                    src="/assets/google.svg"
+                                    loading="lazy"
+                                    alt="google logo"
+                                />
                                 <span>Sign in with Google</span>
                             </button>
                         </div>
-
                         <p className="mt-8 text-xs font-light text-center text-gray-700">
                             {" "}
                             Don't have an account?{" "}
@@ -140,6 +154,5 @@ export default function Login() {
                 </div>
             </animated.div>
         </animated.div>
-
     );
 }
