1bfc3b69427ec3962fd9e694a4ef6a35b267d48b
Vikash Singh <97822077+VikashSingh0007@users.noreply.github.com>
2023-11-24 21:31:22 +0530

    change in footer
    Signed-off-by: Vikash Singh <97822077+VikashSingh0007@users.noreply.github.com>

--- client/src/Components/BreadCrumb.jsx
+++ client/src/Components/BreadCrumb.jsx
@@ -7,7 +7,7 @@ const Breadcrumb = ({ paths }) => {
             <nav className="sm:hidden" aria-label="Back">
                 <Link
                     to={paths[0].to}
-                    className="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700"
+                    className="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 dark:text-white dark:hover:text-white"
                 >
                     <ChevronLeftIcon
                         className="-ml-1 mr-1 h-5 w-5 flex-shrink-0 text-gray-400"
--- client/src/Pages/LandingPage.jsx
+++ client/src/Pages/LandingPage.jsx
@@ -266,7 +266,7 @@ export default function LandingPage() {
                             <div class="mb-6 md:mb-0">
                                 <a href="https://flowbite.com" class="flex items-center">
                                     <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-8" alt="FlowBite Logo" />
-                                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">DivvyUp</span>
                                 </a>
                             </div>
                             <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
